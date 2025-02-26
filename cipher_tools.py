308f5f287c9f9a21f59a59be59e1ed59f76d1005
Damian Flett <damian.flett@outlook.com>
2025-02-26 17:19:58 +1000

    refactoring

--- cipher_tools.py
+++ cipher_tools.py
@@ -1,19 +1,45 @@
 from collections import defaultdict
-#add to general tools
-def letter_to_z26(letter:str):
-    return ord(letter.lower()) - 97
-def z26_to_letter(number:int):
-    return chr(number+97)
 
 #assumption: alphabet is lowercase english alphabet. How can this be generalised?
 
-def apply_affine_cipher(message:str,  a:int, b:int) -> str:
-    encrypted = ""
-    for c in message:
-        encrypted += z26_to_letter((a * letter_to_z26(c)+b) % 26)
-    return encrypted.upper()
-def apply_inverse_affine_cipher(message:str, a:int, b:int) -> str:
-    decrypted = ""
-    for c in message:
-        decrypted += z26_to_letter(((pow(a,-1,26)) * (letter_to_z26(c) - b)) % 26)
-    return decrypted.lower()
+class Cipher():
+    def __init__(self):
+        pass
+    def letter_to_z26(self, letter:str):
+        return ord(letter.lower()) - 97
+
+    def z26_to_letter(self, number:int):
+        return chr(number+97)
+
+class Affine_Cipher(Cipher):
+    def __init__(self, a: int, b: int):
+        self._a = a
+        self._b = b
+    
+    def encrypt(self, message: str) -> str:
+        encrypted = ""
+        for c in message:
+            encrypted += self.z26_to_letter((self._a * self.letter_to_z26(c)+self._b) % 26)
+        return encrypted.upper()
+
+    def decrypt(self, message: str) -> str:
+        decrypted = ""
+        for c in message:
+            decrypted += self.z26_to_letter(((pow(self._a,-1,26)) * (self.letter_to_z26(c) - self._b)) % 26)
+        return decrypted.lower()
+
+class Caesar_Cipher(Cipher):
+    def __init__(self, shift: int):
+        self._shift = shift
+
+    def encrypt(self, message: str) -> str:
+        encrypted = "" 
+        for c in message:
+            encrypted += self.z26_to_letter(self.letter_to_z26(c)+ self._shift)
+        return encrypted.upper()
+
+    def decrypt(self, message: str) -> str:
+        decrypted = "" 
+        for c in message:
+            decrypted += self.z26_to_letter(self.letter_to_z26(c)- self._shift) 
+        return decrypted.lower()
--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-import cipher_tools
+import cipher_tools as ct
 import unittest
 class Test(unittest.TestCase):
     def test_affine(self):
         unenc = "hi"
         enc = "BI"
-        a = 7
-        b=4
-        self.assertEqual(cipher_tools.apply_affine_cipher(unenc,a, b), enc, "hi did not encrypt to BI via affine cipher")
-        self.assertEqual(cipher_tools.apply_inverse_affine_cipher(enc,a,b), unenc,
-        "BI did not convert back to hi via inverse affine cipher")
+        aff = ct.Affine_Cipher(7,4)
+        self.assertEqual(aff.encrypt(unenc), enc, "hi did not encrypt to BI via affine cipher")
+        self.assertEqual(aff.decrypt(enc), unenc, "BI did not convert back to hi via inverse affine cipher")
 
 test = Test()
 test.test_affine()
